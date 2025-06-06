f512e4af5eb429932b464c2d73ee083b121085cb
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 14:12:32 +0530

    Version 1.0

---
+++ Day_1 Task/example_2.java
@@ -0,0 +1,17 @@
+package dsa;
+
+public class example_2 {
+	public static int name(int num) {
+		if(num==5)
+		{
+			return 0;
+		
+		}
+		return num+name(num-1);
+	}
+	public static void main(String []args) {
+		System.out.println(name(10));
+
+}
+
+}
